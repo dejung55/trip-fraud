d05ba7fff2c6d1e3ccf68e9fc29da525fc11eb00
likelion_joonsu <kysc0815@gmail.com>
2016-07-22 08:12:37 +0000

    connect

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,8 +6,42 @@ class HomeController < ApplicationController
     @nation_Asi = Post.all
   end
   
-  def detail
-    @divide = Africa.find(params[:id])
-    
+  def info
+        @every_post = Writing.all
   end
+  
+  def write
+  end
+  
+  def pass
+    new_post = Writing.new
+    new_post.title = params[:title]
+    new_post.content = params[:content]
+    new_post.save
+    
+    redirect_to '/home/info'
+  end
+  
+  def read
+  @every_post= Writing.find(params[:id])  
+  end
+  
+  def destroy
+    @one_post = Writing.find(params[:id])
+    @one_post.destroy
+    redirect_to "/home/info"
+  end
+  
+   def update_view
+    @one_post = Writing.find(params[:id])
+   end
+   
+    def update
+    @one_post = Writing.find(params[:id])
+    @one_post.title = params[:title]
+    @one_post.content = params[:content]
+    @one_post.save
+    redirect_to "/home/info"
+    end
+   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160714192750) do
+ActiveRecord::Schema.define(version: 20160722080132) do
 
   create_table "africas", force: :cascade do |t|
     t.string   "img"
@@ -37,6 +37,13 @@ ActiveRecord::Schema.define(version: 20160714192750) do
   create_table "posts", force: :cascade do |t|
     t.string   "img3"
     t.string   "title3"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "writings", force: :cascade do |t|
+    t.string   "title"
+    t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
