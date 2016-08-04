583a0918cddfcbec3c3ca8ab52d737294044ea74
seobomoon <sbm1992@naver.com>
2016-08-04 09:35:32 +0000

    Sdf

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,45 +4,45 @@
 ##   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.create( id: "0", title3:"일본", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "1", title3:"중국", img3: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-Post.create( id: "2", title3:"필리핀", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "3", title3:"베트남", img3: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
+
+Post.create( id: "0", title3:"일본", img3: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Flag_of_Japan.svg/900px-Flag_of_Japan.svg.png")
+Post.create( id: "1", title3:"중국", img3: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/900px-Flag_of_the_People%27s_Republic_of_China.svg.png")
+Post.create( id: "2", title3:"필리핀", img3: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Flag_of_the_Philippines.svg/900px-Flag_of_the_Philippines.svg.png")
+Post.create( id: "3", title3:"베트남", img3: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Vietnam.svg/900px-Flag_of_Vietnam.svg.png")
 Post.create( id: "4", title3:"인도", img3: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
 Post.create( id: "5", title3:"대만", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "6", title3:"홍콩", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "7", title3:"태국", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "8", title3:"싱가폴", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Post.create( id: "9", title3:"호주", img3: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
+Post.create( id: "6", title3:"홍콩", img3: "https://image-proxy.namuwikiusercontent.com/r/http%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F5%2F5b%2FFlag_of_Hong_Kong.svg%2F200px-Flag_of_Hong_Kong.svg.png")
+Post.create( id: "7", title3:"태국", img3: "https://image-proxy.namuwikiusercontent.com/r/http%3A%2F%2Fwww.worldstatesmen.org%2Fth.gif")
+Post.create( id: "8", title3:"싱가폴", img3: "https://image-proxy.namuwikiusercontent.com/r/http%3A%2F%2Fwww.worldstatesmen.org%2Fsg.gif")
+Post.create( id: "9", title3:"호주", img3: "https://image-proxy.namuwikiusercontent.com/r/http%3A%2F%2Fwww.worldstatesmen.org%2Fau.gif")
 
 
 
-America.create( id: "0", title2:"미국", img2: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-America.create( id: "1", title2:"캐나다", img2: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-America.create( id: "2", title2:"멕시코", img2: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-America.create( id: "3", title2:"페루", img2: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-America.create( id: "4", title2:"아르헨티나", img2: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-America.create( id: "5", title2:"브라질", img2: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
+America.create( id: "0", title2:"미국", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1024px-Flag_of_the_United_States.svg.png")
+America.create( id: "1", title2:"캐나다", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Canada.svg/225px-Flag_of_Canada.svg.png")
+America.create( id: "2", title2:"멕시코", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/225px-Flag_of_Mexico.svg.png")
+America.create( id: "3", title2:"페루", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Flag_of_Peru.svg/225px-Flag_of_Peru.svg.png")
+America.create( id: "4", title2:"아르헨티나", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/225px-Flag_of_Argentina.svg.png")
+America.create( id: "5", title2:"브라질", img2: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/05/Flag_of_Brazil.svg/225px-Flag_of_Brazil.svg.png")
 
 
+Europe.create( id: "0", title1:"프랑스", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/225px-Flag_of_France.svg.png")
+Europe.create( id: "1", title1:"영국", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Flag_of_the_United_Kingdom.svg/225px-Flag_of_the_United_Kingdom.svg.png")
+Europe.create( id: "2", title1:"독일", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Flag_of_Germany.svg/225px-Flag_of_Germany.svg.png")
+Europe.create( id: "3", title1:"폴란드", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/225px-Flag_of_Poland.svg.png")
+Europe.create( id: "4", title1:"이탈리아", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/225px-Flag_of_Italy.svg.png")
+Europe.create( id: "5", title1:"스위스", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Flag_of_Switzerland_%28Pantone%29.svg/225px-Flag_of_Switzerland_%28Pantone%29.svg.png")
+Europe.create( id: "6", title1:"스페인", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Flag_of_Spain.svg/225px-Flag_of_Spain.svg.png")
+Europe.create( id: "7", title1:"체코", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_Czech_Republic.svg/225px-Flag_of_the_Czech_Republic.svg.png")
+Europe.create( id: "8", title1:"오스트리아", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/225px-Flag_of_Austria.svg.png")
+Europe.create( id: "9", title1:"포르투갈", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/225px-Flag_of_Portugal.svg.png")
+Europe.create( id: "10", title1:"터키", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b4/Flag_of_Turkey.svg/225px-Flag_of_Turkey.svg.png")
+Europe.create( id: "11", title1:"러시아", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Russia.svg/225px-Flag_of_Russia.svg.png")
+Europe.create( id: "12", title1:"네덜란드", img1: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/225px-Flag_of_the_Netherlands.svg.png")
 
-Europe.create( id: "0", title1:"프랑스", img1: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Europe.create( id: "1", title1:"영국", img1: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
-Europe.create( id: "2", title1:"독일", img1: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-Europe.create( id: "3", title1:"폴란드", img1: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Europe.create( id: "4", title1:"이탈리아", img1: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
-Europe.create( id: "5", title1:"스위스", img1: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-Europe.create( id: "6", title1:"스페인", img1: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Europe.create( id: "7", title1:"체코", img1: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
-Europe.create( id: "8", title1:"오스트리아", img1: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-Europe.create( id: "9", title1:"포르투갈", img1: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
-Europe.create( id: "10", title1:"터키", img1: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
-Europe.create( id: "11", title1:"러시아", img1: "http://www.icons.or.kr/attach/board/migration/icons/cooper371/1243489097.02.jpg")
-Europe.create( id: "12", title1:"네덜란드", img1: "http://cdnweb01.wikitree.co.kr/webdata/editor/201511/24/img_20151124142442_ed30474f.png")
 
-
-Africa.create( id: "0", title:"이집트", img: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
-Africa.create( id: "1", title:"남아공", img: "http://cfile208.uf.daum.net/image/211DBE43510B5F4E2DF8B5")
+Africa.create( id: "0", title:"이집트", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/225px-Flag_of_Egypt.svg.png")
+Africa.create( id: "1", title:"남아공", img: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/225px-Flag_of_South_Africa.svg.png")
 
 Asia1.create( asiaimg: "/assets/map/asia/japan.png", post_id: 0)
 Asia1.create( asiaimg: "/assets/map/asia/china.png", post_id: 1)
@@ -60,5 +60,8 @@ Twitter.create(random: "수준")
 Twitter.create(random: "사자")
 Twitter.create(random: "자전거")
 Twitter.create(random: "거미")
+
+
+
 
 
