875670111c67e29df08d766c699d0c3a76aa6ee9
seobomoon <sbm1992@naver.com>
2016-08-24 04:35:17 +0000

    devise

--- config/initializers/aws.rb
+++ config/initializers/aws.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAJAO3M6NGVB4QCWFA',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     '',                        # required
+    aws_secret_access_key: '',                        # required
     region:                'ap-northeast-2',                  # optional, defaults to 'us-east-1'
     endpoint:              'https://s3.ap-northeast-2.amazonaws.com' # optional, defaults to nil
   }
